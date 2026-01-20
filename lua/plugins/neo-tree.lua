89b2fd4a726e94369f6cb29fa3f143eded65fd19
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2026-01-20 12:35:05 +0600

    extract plugins to separate file

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  cmd = "Neotree",
+  keys = {
+    { "\\", ":Neotree toggle<CR>", desc = "NeoTree reveal", silent = true },
+  },
+  opts = {
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+      window = {
+        width = 30,
+        position = "right",
+      },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+    buffers = { follow_current_file = { enable = true } },
+  },
+}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = { enabled = false },
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
