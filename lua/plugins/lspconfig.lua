d0819780efb480bc4faa84a061f396f9f64cffe2
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2026-02-03 14:36:27 +0600

    (eslint) automatically fix errors on save

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -12,5 +12,13 @@ return {
         },
       },
     },
+    setup = {
+      eslint = function()
+        -- automatically fix linting errors on save (but otherwise do not format the document)
+        vim.cmd([[
+          autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js LspEslintFixAll
+        ]])
+      end,
+    },
   },
 }
