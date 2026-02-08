16cfbb125f4568f8c28ec56a9fbac7a30af9c484
Md. Shahnewaz Siddique <Md.Shahnewaz.Siddique@optimizely.com>
2026-02-08 20:52:23 +0600

    add nvim-spider

---
+++ lua/plugins/nvim-spider.lua
@@ -0,0 +1,15 @@
+return {
+  "chrisgrieser/nvim-spider",
+  keys = {
+    { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+    { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+    { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+    { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+  },
+  setup = {
+    skipInsignificantPunctuation = true,
+    subwordMovement = true,
+    consistentOperatorPending = false, -- see the README for details
+    customPatterns = {}, -- see the README for details
+  },
+}
